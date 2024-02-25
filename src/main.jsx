a0968c5f5488d77f19d0d6508c48dfa1bdfd76be
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-02-25 17:08:24 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import ReactDOM from 'react-dom/client'
+
+import '../src/assets/scss/App.scss';
+
+
+import { BrowserRouter } from 'react-router-dom';
+import App from "./App";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
