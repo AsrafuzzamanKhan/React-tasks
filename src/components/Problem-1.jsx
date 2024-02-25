dd7fe81c87f8ae7bd48ee8af59f6a9fa237fe2e4
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-02-25 18:23:11 +0600

    todo

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,25 +1,49 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
 
-    const handleClick = (val) =>{
+    const [todos, setTodos] = useState([]);
+
+    const [todosInfo, setTodosInfo] = useState([]);
+
+    // const active = todos.filter(td => td.status === 'active')
+    const handleClick = (val) => {
+
         setShow(val);
+        // setShow(val.active)
+    }
+    const onInputChange = e => {
+        e.preventDefault();
+        const field = e.target.name;
+        const value = e.target.value;
+        const newTodo = { ...todosInfo }
+        newTodo[field] = value;
+        console.log('new todo', newTodo);
+        setTodosInfo(newTodo)
+
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        setTodos([...todos, todosInfo])
+        setTodosInfo('')
+        console.log(todos.length);
     }
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+                <h4 className='text-center text-uppercase mb-5'>Problem-1 </h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={onFormSubmit}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" className="form-control" placeholder="Name" name='name' onBlur={onInputChange} required />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" className="form-control" placeholder="Status" name='status' onBlur={onInputChange} required />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,25 +53,28 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>Completed</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {todos?.map((todo, i) => (<tr key={i}>
+                                <th>{todo?.name}</th>
+                                <th>{todo?.status}</th>
+                            </tr>))}
                         </tbody>
                     </table>
                 </div>
